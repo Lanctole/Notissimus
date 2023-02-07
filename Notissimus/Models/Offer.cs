4bc7480bf35633a20689e3061ff7af67a3238c70
55445592+Lanctole@users.noreply.github.com <55445592+Lanctole@users.noreply.github.com>
2023-02-07 13:21:14 +0300

    Добавил класс,  условно описывающий заказ

---
+++ Notissimus/Models/Offer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Notissimus.Models
+{
+    public class Offer
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Bid { get; set; }
+        public int? CBid { get; set; }
+        public bool Available { get; set; }
+        public string? Type { get; set; }
+        public string? OtherOfferProperties { get; set; }
+        
+    }
+}
