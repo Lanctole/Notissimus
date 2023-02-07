9f2571c929e91f1f8b32a1199544db1d5d6fe976
55445592+Lanctole@users.noreply.github.com <55445592+Lanctole@users.noreply.github.com>
2023-02-07 13:21:29 +0300

    Добавил модель подключения к БД

---
+++ Notissimus/Models/NotissimusDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Notissimus.Models
+{
+    public class NotissimusDbContext : DbContext
+    {
+        public NotissimusDbContext(DbContextOptions<NotissimusDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Offer> Offers { get; set; } = null!;
+    }
+}
